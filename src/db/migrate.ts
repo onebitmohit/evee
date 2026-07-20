d31033b195d71ef22770832d9b4041cc0b5b22fd
Mohit-658 <mohit.sharma80805@gmail.com>
2026-07-20 15:42:09 +0530

    remove src/db/migrate.ts

--- src/db/migrate.ts
+++
@@ -1,5 +0,0 @@
-import { migrate } from "drizzle-orm/libsql/migrator";
-import { db } from "./client";
-
-await migrate(db, { migrationsFolder: "./drizzle" });
-console.log("Database migrations applied.");
